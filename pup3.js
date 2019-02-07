48d70df3698f26a7842d8cd8261d1d9cdde44db1
Bill Withers <wither7007@gmail.com>
2019-02-07 12:34:51 -0600

    works with try

--- pup3.js
+++ pup3.js
@@ -11,19 +11,25 @@ const devices = require("puppeteer/DeviceDescriptors")
 
 ;
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: true
-    })
-    const page = await browser.newPage()
-    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
+    try {
 
-    //   await page.emulate(devices["iPhone 6"])
-    await page.goto("https://experts.shopify.com", {
-        timeout: 0
-    })
-    await page.waitForSelector(".section")
-    console.log("it's showing   ")
-    const sections = await page.$$('.section')
-    console.log(sections.length)
-    await browser.close()
+
+        const browser = await puppeteer.launch({
+            headless: true
+        })
+        const page = await browser.newPage()
+        await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
+
+        //   await page.emulate(devices["iPhone 6"])
+        await page.goto("https://experts.shopify.com", {
+            timeout: 0
+        })
+        await page.waitForSelector(".section")
+        console.log("it's showing   ")
+        const sections = await page.$$('.section')
+        console.log(sections.length)
+        await browser.close()
+    } catch (e) {
+        console.log('our error', e)
+    }
 })()
