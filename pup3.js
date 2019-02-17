1212f64a15ec25aa81ce26c12093c81cc3ba3e93
Bill Withers <wither7007@gmail.com>
2019-02-17 14:00:17 -0600

    added remote

--- pup3.js
+++ pup3.js
@@ -15,7 +15,7 @@ const devices = require("puppeteer/DeviceDescriptors")
 
 
         const browser = await puppeteer.launch({
-            headless: true
+            headless: false
         })
         const page = await browser.newPage()
         await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 9_0_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A404 Safari/601.1')
@@ -28,6 +28,18 @@ const devices = require("puppeteer/DeviceDescriptors")
         console.log("it's showing   ")
         const sections = await page.$$('.section')
         console.log(sections.length)
+
+        for (const section of sections) {
+            const button = await section.$('a.marketing-button')
+            button.click()
+            await page.waitForSelector('#ExpertsResult')
+            const lis = await page.$$('#ExpertsResult li')
+            for (const li of lis) {
+                const name = li.$eval('h2', h2 => h2.innerText)
+                console.log('name', name)
+            }
+
+        }
         await browser.close()
     } catch (e) {
         console.log('our error', e)
---
+++ v
@@ -0,0 +1,15 @@
+GNU bash, version 4.3.48(1)-release (x86_64-pc-linux-gnu)
+Copyright (C) 2013 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+
+This is free software; you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+4.3.48(1)-release
+apt-vim
+bin
+dotfiles
+githist
+gl
+handBack
+junk
+pw2
